7173b4cc9044ea20e68f1297e59508280d7991a2
Mitchell Krog <mitchellkrog@gmail.com>
2017-02-16 15:50:50 +0200

    Tidy up travis.yml a bit

---
+++ travisCI/install-apache.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+############################################################
+# Let us Setup Apache2 for Ubuntu 16.04 with Php 7 and MySQL
+############################################################
+# Install Apache2
+sudo apt-get update
+sudo apt-get install -y --force-yes apache2
+# Add PHP 7.0 Repository
+sudo add-apt-repository -y ppa:ondrej/php
+# Install PHP 7.0
+sudo apt-get update
+sudo apt-get install -y --force-yes php7.0 libapache2-mod-php7.0 php7.0-fpm
+# Copy our virtual host template to sites-enabled overwriting the default site conf
+sudo cp travisCI/defaultsite.tpl /etc/apache2/sites-available/000-default.conf
+# Copy basic testing files into /var/www
+sudo cp www/info.php /var/www/info.php
+sudo cp www/apache.php /var/www/apache.php
+# Enable mod rewrite module
+sudo a2enmod rewrite
+# Set ServerName Globally
+sudo cp travisCI/servername.tpl /etc/apache2/conf-available/servername.conf
+# Add testing of Apache Bad Bot Blocker
+sudo mkdir /etc/apache2/custom.d
+cd /etc/apache2/custom.d
+sudo wget https://raw.githubusercontent.com/mitchellkrogza/apache-ultimate-bad-bot-blocker/master/custom.d/globalblacklist.conf
+sudo a2enconf servername
+# Restart apache
+sudo service apache2 restart
+# Restart PHP
+sudo service php7.0-fpm restart
